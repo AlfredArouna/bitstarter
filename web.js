d5332719d052c9b622e41f3831f92ff57a75eb75
Alfred AROUNA <alfred.arouna@gmail.com>
2013-07-05 20:57:05 +0000

    Modify web.js to be able to get content from index.html

--- web.js
+++ web.js
@@ -2,9 +2,22 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+var fs = require('fs');
+
+fs.readFile('index.html','utf8','r', function (err, data) {
+  if (err) throw err;
+ // console.log(data);
+ app.get('/', function(request, response) {
+  response.send('Hello World!');
+ });
+
+});
+
+/*
 app.get('/', function(request, response) {
   response.send('Hello World!');
 });
+*/
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
